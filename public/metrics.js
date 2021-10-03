b924474ca988d3f432966ac112add3cdc8937147
riksq <91621987+riksq@users.noreply.github.com>
2021-10-03 16:29:51 +0300

    Update metrics.js

--- public/metrics.js
+++ public/metrics.js
@@ -1,14 +1,9 @@
-const headScript = '<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KB8CSW8');</script>'; 
+const headScript = 'body'; 
 
 // Заменить на код, который даёт гугл для вставки в <head>
-const bodyScript = '<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KB8CSW8"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>';
+const bodyScript = 'head';
     
-    // Заменить на код, который даёт гугл для вставки в <body>
+// Заменить на код, который даёт гугл для вставки в <body>
 
 var enableMetrics = ( ) => {
     console.log("Not Google Headless. Enabling metrics..");
