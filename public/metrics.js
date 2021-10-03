dad043eac2b47ccc34fb8e9aacb6d78b084b2e9d
riksq <91621987+riksq@users.noreply.github.com>
2021-10-03 15:39:27 +0300

    Update metrics.js

--- public/metrics.js
+++ public/metrics.js
@@ -1,5 +1,14 @@
-const headScript = '<!-- HEAD METRIC SCRIPT -->'; // Заменить на код, который даёт гугл для вставки в <head>
-const bodyScript = '<!-- BODY METRIC SCRIPT -->'; // Заменить на код, который даёт гугл для вставки в <body>
+const headScript = '<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KB8CSW8');</script>
+<!-- End Google Tag Manager -->'; // Заменить на код, который даёт гугл для вставки в <head>
+const bodyScript = '<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KB8CSW8"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->'; // Заменить на код, который даёт гугл для вставки в <body>
 
 var enableMetrics = ( ) => {
     console.log("Not Google Headless. Enabling metrics..");
