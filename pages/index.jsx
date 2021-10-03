81081e05c971b841e404e43335e67955f720cfa2
riksq <91621987+riksq@users.noreply.github.com>
2021-10-03 15:37:38 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import TagManager from "react-gtm-module";
+//import TagManager from "react-gtm-module";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
@@ -47,15 +47,15 @@ export default function Home(props) {
         }
     };
 
-    useEffect(() => {
+  //  useEffect(() => {
        // alert(gtm);
         // TagManager.initialize({ gtmId: "GTM-PTZV39P" });
         // TagManager.initialize({ gtmId: "GTM-59WP5XJ" });
-        TagManager.initialize({ gtmId: "GTM-PGRFKT5" });
+     //   TagManager.initialize({ gtmId: "GTM-PGRFKT5" });
        // if (gtm) {
           //  TagManager.initialize({ gtmId: gtm });
        // }
-    }, [gtm]);
+ //   }, [gtm]);
 
     useEffect(() => {
         const script = document.createElement('script');
