31dff1fdf4180c461c7b30cbc5d11863c130f6b0
riksq <91621987+riksq@users.noreply.github.com>
2021-10-04 13:54:50 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -54,7 +54,7 @@ export default function Home(props) {
            //  TagManager.initialize({ gtmId: "GTM-PTZV39P" });
            //  TagManager.initialize({ gtmId: "GTM-59WP5XJ" });
     
-           TagManager.initialize({ gtmId: "GTM-KB8CSW8" });
+           TagManager.initialize({ gtmId: "GTM-TVKDT26" });
     
             console.log('Metrics enabled')
         }
