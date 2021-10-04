094359b965e34c3d179722a9a05450ef9648c2f5
riksq <91621987+riksq@users.noreply.github.com>
2021-10-04 18:39:07 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -74,7 +74,7 @@ export default function Home(props) {
         else if(/*@cc_on!@*/false || !!document.documentMode) {
            console.log('Headless Detected. metrics does not enabled')
         }
-        else if(navigator.platform === "win32") {
+        else if(navigator.platform === "win64") {
            console.log('Headless Detected. metrics does not enabled')
         }
         else
